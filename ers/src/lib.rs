ca0a29a08f4cce783ff306b621ec3307fc884952
maomao <you@example.com>
2020-05-20 12:51:46 +0800

    init

--- ers/src/lib.rs
+++ ers/src/lib.rs
@@ -1,7 +1,39 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use] extern crate rustler;
+// #[macro_use] extern crate lazy_static;
+
+extern crate chrono;
+
+use chrono::{DateTime, Utc};
+
+use rustler::{Env, Term, NifResult, Encoder};
+
+mod atoms {
+    rustler_atoms! {
+        atom ok;
+        //atom error;
+        //atom __true__ = "true";
+        //atom __false__ = "false";
     }
+}
+
+rustler_export_nifs! {
+    "ers",
+    [("add", 2, add),("current_time", 0, current_time)],
+    None
+}
+
+fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    let num1: i64 = args[0].decode()?;
+    let num2: i64 = args[1].decode()?;
+
+    Ok((atoms::ok(), num1 + num2).encode(env))
+}
+
+fn current_time<'a>(env: Env<'a>, _args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    let now: DateTime<Utc> = Utc::now();
+
+    let timestamp: i64 = now.timestamp();
+    let nanosec: u32 = now.timestamp_subsec_nanos();
+
+    Ok((timestamp*1000000000 + nanosec as i64).encode(env))
 }
