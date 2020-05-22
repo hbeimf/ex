7f52649ec4fbb69c0eb84c81431c43a9c8f8737d
maomao <you@example.com>
2020-05-22 15:29:18 +0800

    pu

--- ers/src/map.rs
+++ ers/src/map.rs
@@ -14,29 +14,35 @@ pub fn echo_map<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     Ok(r.encode(env))
 }
 
-//
-
-
-
+//tt:map_list().
 pub fn map_list<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     let iter: ListIterator = args[0].decode()?;
 
-    let _res: Result<Vec<AMap>, Error> = iter
+    let res: Result<Vec<AMap>, Error> = iter
         .map(|x| x.decode::<AMap>())
         .collect();
 
-//    println!("{:?}", res);
+    match res {
+        Ok(result) => {
+            for elem in result.iter() {
+                println!("map:{:?}", elem);
+            }
 
-//    match res {
-//        Ok(result) => Ok(result.iter().map(|x| x.encode(env)).encode(env)),
-//        Err(err) => Err(err),
-//    }
-
-//    match res {
-//        Ok(result) => Ok(result.iter().fold(0, |acc, &x| acc + x).encode(env)),
-//        Err(err) => Err(err),
-//    }
+        },
+        Err(_err) => {
+//            Err(err)
+            ()
+        },
+    };
 
 
-    Ok(args[0].encode(env))
+    let m1 = AMap{a: 10, b: 11};
+    let m2 = AMap{a: 20, b: 21};
+    let list = vec![m1, m2];
+
+//    Ok(args[0].encode(env))
+//    let list = vec![1, 2, 3];
+    Ok(list.encode(env))
+
+
 }
