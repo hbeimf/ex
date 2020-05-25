c7fb7b6035ef3b54c5ece60e92d1f85783917490
maomao <you@example.com>
2020-05-25 14:20:47 +0800

    pu

---
+++ ex_use_ers/apps/ers/lib/ers/ers/simple_actor.ex
@@ -0,0 +1,25 @@
+defmodule Ers.SimpleActor do
+  @moduledoc false
+  
+
+
+  use GenServer
+
+  def start_link do
+    state = :ok
+    opts = []
+    GenServer.start_link(__MODULE__, state, opts)
+  end
+
+  def init(_opts) do
+    {:ok, %{}}
+  end
+
+  def handle_call(_msg, _from, state) do
+    {:reply, :ok, state}
+  end
+
+  def handle_cast(_msg, state) do
+    {:noreply, state}
+  end
+end
--- ex_use_ers/apps/ers/lib/ers/ers/use_simple_sup.ex
+++ ex_use_ers/apps/ers/lib/ers/ers/use_simple_sup.ex
@@ -1,20 +1,41 @@
 defmodule Ers.UseSimpleSup.Supervisor do
   @moduledoc false
-  
 
+  @name __MODULE__
 
   use Supervisor
 
   def start_link(arg) do
-    Supervisor.start_link(__MODULE__, arg)
+    Supervisor.start_link(__MODULE__, arg, name: @name)
   end
 
-  def init(arg) do
-    children = [
-#      worker(MyWorker, [arg], restart: :temporary)
-    ]
+#  def init(arg) do
+#    children = [
+##      worker(MyWorker, [arg], restart: :temporary)
+#    ]
+#
+#    supervise(children, strategy: :one_for_one)
+#  end
 
-    supervise(children, strategy: :one_for_one)
+  def init(_args) do
+    import Supervisor.Spec
+
+#    {m, f, a} = {Ers.SimpleActor, :start_link, []}
+    worker_opts = [restart:  :permanent,
+      shutdown: 5000,
+      function: :start_link]
+
+    children = [worker(Ers.SimpleActor, [], worker_opts)]
+    opts     = [strategy:     :simple_one_for_one,
+      max_restarts: 5,
+      max_seconds:  5]
+
+    supervise(children, opts)
+  end
+
+#  Ers.UseSimpleSup.Supervisor.start_actor()
+  def start_actor() do
+    Supervisor.start_child(Ers.UseSimpleSup.Supervisor, [])
   end
 
 end
